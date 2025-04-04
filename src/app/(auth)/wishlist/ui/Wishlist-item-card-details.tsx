f4faa0a441f0be9fd227c75784dbc3c3b6422333
Etore Grande <etore@MacBook-Pro-de-Etore.local>
2025-04-04 15:17:56 -0300

    Added WishlistItemCardDetails modal

--- src/app/(auth)/wishlist/ui/Wishlist-item-card-details.tsx
+++ src/app/(auth)/wishlist/ui/Wishlist-item-card-details.tsx
@@ -1,25 +1,40 @@
-// 'use client'
+'use client'
 
-// import { WishlistItem } from "@/types/wishlistItem"
-// import { setImageSrc, useGetContext } from "../actions"
-// import { WishlistContext } from "../context/Wishlist-context"
+import { WishlistItem } from "@/types/wishlistItem"
+import { setImageSrc, useGetContext } from "../actions"
+import { WishlistContext } from "../context/Wishlist-context"
+import { Input } from "@/components/ui/input"
 
-// interface WishlistItemCardDetailProps {
-//     wishlistItem: WishlistItem
-//     isOpen: boolean
-// }
+interface WishlistItemCardDetailProps {
+    wishlistItem: WishlistItem | undefined
+}
 
-// export const WishlistItemCardDetail = ({ wishlistItem, isOpen }: WishlistItemCardDetailProps) => {
-//     if (!isOpen || !wishlistItem) return null
+export const WishlistItemCardDetails = ({ wishlistItem }: WishlistItemCardDetailProps) => {
+    if (!wishlistItem) return null
 
-//     const { router } = useGetContext(WishlistContext)
+    const { router } = useGetContext(WishlistContext)
 
-//     const imageSrc = setImageSrc(wishlistItem)
+    const imageSrc = setImageSrc(wishlistItem)
 
-//     return (
-//         <>
+    return (
 
-//         </>
-//     )
-// }
+        <div
+            onClick={() => router.push('/wishlist', { scroll: false })}
+            className="fixed inset-0 z-50 flex items-center justify-center bg-[rgba(0,0,0,0.5)]"
+        >
+            <div
+                onClick={(e) => e.stopPropagation()}
+                className="bg-white p-6 rounded-2xl shadow-lg w-full max-w-md"
+            >
+                <h2 className="">{wishlistItem?.name}</h2>
+                <Input
+                    name="name"
+                    placeholder=""
+                    defaultValue={String(wishlistItem?.name)} />
+            </div>
+        </div>
+
+
+    )
+}
 
--- src/app/(auth)/wishlist/ui/Wishlist.tsx
+++ src/app/(auth)/wishlist/ui/Wishlist.tsx
@@ -9,6 +9,10 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { createWishlistItemFormSchema } from "@/schemas/wishlistItem";
 import { CreateWishlistItemButton } from "./Create-wishlist-item-button";
 import { EmptyWishlist } from "./Wishlist-empty";
+import { useSearchParams } from "next/navigation";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+import { WishlistItemCardDetails } from "./Wishlist-item-card-details";
 
 interface WishlistProps {
     userId?: string,
@@ -16,6 +20,10 @@ interface WishlistProps {
 }
 
 export function Wishlist({ initialWishlist }: WishlistProps) {
+    const searchParams = useSearchParams()
+    const router = useRouter()
+    const itemId = searchParams.get("item")
+
     const formHook = useForm<CreateWishlistItemFormDataType>({
         resolver: zodResolver(createWishlistItemFormSchema),
         defaultValues: {
@@ -28,12 +36,27 @@ export function Wishlist({ initialWishlist }: WishlistProps) {
         }
     });
 
+    useEffect(() => {
+        if (itemId) {
+            document.body.classList.add("overflow-hidden");
+        } else {
+            document.body.classList.remove("overflow-hidden");
+        }
+
+        return () => {
+            document.body.classList.remove("overflow-hidden");
+        };
+    }, [itemId]);
+
     const isEmpty = initialWishlist.length === 0;
+    const openedWishlistItem = initialWishlist.find((wishlistItem) => wishlistItem.id === itemId)
 
     return (
         <>
             <WishlistContextProvider>
                 <FormProvider {...formHook}>
+                    {/* Modal component */}
+                    <WishlistItemCardDetails wishlistItem={openedWishlistItem} />
                     <CreateWishlistItemButton />
                     <EmptyWishlist isEmpty={isEmpty} />
                     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4"  >
