81b710ba31969b8fb3c14b4311ec24bfeccc73b3
Etore Grande <etoregrande@gmail.com>
2025-04-05 11:05:23 -0300

    Fixed description lint issues

--- src/app/(auth)/wishlist/ui/Wishlist-empty.tsx
+++ src/app/(auth)/wishlist/ui/Wishlist-empty.tsx
@@ -1,11 +1,15 @@
+import { useGetContext } from "../actions"
+import { WishlistContext } from "../context/Wishlist-context"
+
 type EmptyWishlistProps = {
     isEmpty: boolean
 }
 
 export const EmptyWishlist = ({ isEmpty }: EmptyWishlistProps) => {
+    const { newItem } = useGetContext(WishlistContext)
 
     return (
-        isEmpty &&
+        isEmpty && !newItem &&
         <div className="flex justify-center min-h-60">
             <p>Sua lista de desejos está vazia!</p>
         </div>
