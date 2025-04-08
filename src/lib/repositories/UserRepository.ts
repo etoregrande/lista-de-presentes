69d046106ed8ee52be011d5fde0f73e7baf852a6
Etore Grande <etoregrande@gmail.com>
2025-04-08 16:51:37 -0300

    fixed getUser function

--- src/lib/repositories/UserRepository.ts
+++ src/lib/repositories/UserRepository.ts
@@ -1,11 +1,12 @@
 import { db } from '@/lib/database/db';
 
-export async function getUser(userId?: string, email?: string) {
-    if (!userId && !email) throw new Error('You must provide a userId or email')
+export async function getUserById(userId?: string) {
+    if (!userId) throw new Error('You must provide a userId')
 
-    const query = db.selectFrom('user')
-    if (userId) query.where('id', '=', userId)
-    if (email) query.where('email', '=', email)
+    const user = db.selectFrom('user')
+        .where("id", "=", userId)
+        .selectAll()
+        .executeTakeFirst();
 
-    return await query.selectAll().executeTakeFirst();
+    return user
 }
