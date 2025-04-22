abab39c819e6f1a41a5bb52797c500e03ebb8261
Etore Grande <etoregrande@gmail.com>
2025-04-22 10:35:18 -0300

    New shared wishlist content layout

--- src/app/(auth)/wishlist/shared/[userId]/ui/Wishlist-shared-item-sheet-content.tsx
+++ src/app/(auth)/wishlist/shared/[userId]/ui/Wishlist-shared-item-sheet-content.tsx
@@ -3,6 +3,8 @@ import { Dispatch, SetStateAction } from 'react'
 import { AspectRatio } from '@/components/ui/aspect-ratio'
 import { WishlistItem } from '@/types/db'
 import placeholder from '@/../public/assets/wishlist-item-placeholder.svg'
+import { ExternalLink, Link2Off } from 'lucide-react'
+import { Label } from '@/components/ui/label'
 
 interface WishlistSharedItemSheetContentProps {
   wishlistItem: Partial<WishlistItem>
@@ -27,18 +29,33 @@ export const WishlistSharedItemSheetContent = ({
           />
         </AspectRatio>
 
-        {wishlistItem.description ? (
-          <p className="text-sm break-words">{wishlistItem.description}</p>
-        ) : (
-          <></>
-        )}
+        <div className="space-y-2">
+          <Label>Descrição</Label>
+          {wishlistItem.description ? (
+            <p className="text-muted-foreground break-words">
+              "{wishlistItem.description}"
+            </p>
+          ) : (
+            <p className="text-muted-foreground text-sm break-words">
+              Produto sem descrição
+            </p>
+          )}
+        </div>
 
         {wishlistItem.link ? (
-          <a href={wishlistItem.link} target="_blank">
-            Link do produto
+          <a
+            href={wishlistItem.link}
+            target="_blank"
+            className="border-border hover:bg-primary-foreground hover:border-primary-foreground transition-[background-color, border] flex gap-4 rounded-md border p-4 duration-200"
+          >
+            <ExternalLink className="text-primary" />
+            <p>Link para comprar</p>
           </a>
         ) : (
-          <></>
+          <div className="bg-muted flex gap-4 rounded-md p-4 duration-200">
+            <Link2Off className="text-muted-foreground" />
+            <p className="text-muted-foreground">Sem link para comprar</p>
+          </div>
         )}
       </div>
     </div>
