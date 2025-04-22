562c54a0ccdcaf0c2a9642c995d6d6481c66cb3d
Etore Grande <etoregrande@gmail.com>
2025-04-22 17:18:00 -0300

    testint tailwind interpolation bug

--- src/app/(auth)/wishlist/shared/[userId]/ui/Wishlist-shared-item-sheet-trigger.tsx
+++ src/app/(auth)/wishlist/shared/[userId]/ui/Wishlist-shared-item-sheet-trigger.tsx
@@ -16,10 +16,10 @@ export const WishlistSharedItemSheetTrigger = ({
     <motion.div
       layout
       initial={{ opacity: 0, scale: 0.95 }}
-      animate={{
-        opacity: wishlistItem.is_purchased ? 0.4 : 1,
-        scale: 1,
-      }}
+      // animate={{
+      //   opacity: wishlistItem.is_purchased ? 0.4 : 1,
+      //   scale: 1,
+      // }}
       exit={{ opacity: 0, scale: 0.95 }}
       transition={{
         duration: 0.2,
