b4bc4f468399f411c3f9b61413750863cee328a6
Etore Grande <etoregrande@gmail.com>
2025-04-24 15:34:08 -0300

    Restructured migrations, redesigned toasts

--- src/app/(public)/login/ui/Login-wrapper.tsx
+++ src/app/(public)/login/ui/Login-wrapper.tsx
@@ -57,7 +57,7 @@ export function LoginWrapper({ className }: LoginWrapperProps) {
         </p>
 
         {formType === 'login' && <LoginForm setFormType={setFormType} />}
-        {formType === 'register' && <SignUpForm />}
+        {formType === 'register' && <SignUpForm setFormType={setFormType} />}
         {formType === 'forgot-password' && (
           <ForgotPasswordForm setFormType={setFormType} />
         )}
