b2b879ce9b92a01b109780dacfc43df4bcb1508f
Etore Grande <etoregrande@gmail.com>
2025-04-26 15:56:31 -0300

    Arrumada larguraa do componente de login

--- src/app/(public)/login/page.tsx
+++ src/app/(public)/login/page.tsx
@@ -6,7 +6,7 @@ export default function Page() {
   return (
     <div className="bg-[image:var(--gradient)]">
       <div className="mx-auto flex min-h-dvh flex-col items-center justify-center px-2 sm:max-w-4xl sm:px-10">
-        <div className="flex w-sm flex-col rounded-2xl bg-white shadow-2xl md:h-[600px] md:w-full md:flex-row">
+        <div className="flex flex-col rounded-2xl bg-white shadow-2xl md:h-[600px] md:w-full md:flex-row">
           <div className="hidden w-full p-2 md:block md:w-1/2">
             <div className="relative flex h-full w-full flex-col justify-between overflow-hidden rounded-lg p-4">
               <Image
