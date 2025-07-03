cb7c0d9270792df87db0c6e5a361aab7306ea779
Etore Grande <etoregrande@gmail.com>
2025-07-03 15:15:22 -0300

    Fixed middleware linting

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
-import { NextRequest, NextResponse } from 'next/server'
-import { getSessionCookie } from 'better-auth/cookies'
+import { /*NextRequest*/ NextResponse } from 'next/server'
+// import { getSessionCookie } from 'better-auth/cookies'
 
-export async function middleware(request: NextRequest) {
+export async function middleware(/*request: NextRequest*/) {
   // const sessionCookie = getSessionCookie(request, {
   //   // Optionally pass config if cookie name, prefix or useSecureCookies option is customized in auth config.
   //   cookieName: 'session_token',
