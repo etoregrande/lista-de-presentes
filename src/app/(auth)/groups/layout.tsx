bdf98c7f687a90fd4b259762a81f4fa39ccd17d4
Etore Grande <etoregrande@gmail.com>
2025-07-04 15:03:43 -0300

    New groups page

--- src/app/(auth)/groups/layout.tsx
+++ src/app/(auth)/groups/layout.tsx
@@ -9,21 +9,7 @@ export default async function WishlistLayout({
   return (
     <>
       <Navbar />
-      <Image
-        src="/assets/wishlist/gift-bg.svg"
-        alt="imagem de fundo"
-        width={600}
-        height={600}
-        className="fixed -bottom-40 -left-40 -z-10 hidden opacity-50 lg:block"
-      />
-      <Image
-        src="/assets/wishlist/gift2-bg.svg"
-        alt="imagem de fundo"
-        width={600}
-        height={600}
-        className="fixed -right-40 -bottom-40 -z-10 hidden opacity-50 lg:block"
-      />
-      <div className="layout-container">{children}</div>
+      {children}
     </>
   )
 }
--- src/app/(auth)/groups/page.tsx
+++ src/app/(auth)/groups/page.tsx
@@ -8,5 +8,50 @@ export default async function Page() {
 
   if (!session) redirect('/login')
 
-  return <p>Sua lista de grupos de amigo secreto</p>
+  return (
+    <>
+      <header className="layout-container flex justify-between py-10">
+        <div className="grid w-full grid-cols-[auto_1fr] gap-4">
+          <div className="aspect-square w-full rounded-md bg-amber-300"></div>
+          <h1 className="text-2xl font-bold">
+            <span className="block text-base font-normal text-slate-500">
+              Amigo secreto
+            </span>
+            Nome do grupo
+          </h1>
+        </div>
+        <div className="flex w-full flex-col items-end">
+          <p className="text-muted-foreground">Evento dia 24/12</p>
+          <p>
+            Presentes até <span className="font-bold">R$100,00</span>
+          </p>
+        </div>
+      </header>
+
+      <main className="layout-container flex flex-col gap-10 md:flex-row">
+        <section className="flex w-full flex-col gap-10 md:w-2/3">
+          <article className="grid w-full gap-2">
+            <h2 className="text-lg font-bold">Quem eu tirei</h2>
+            <div className="bg-muted flex min-h-40 w-full items-center justify-center rounded-md">
+              <p>O sorteio ainda não foi realizado!</p>
+            </div>
+          </article>
+          <article className="grid w-full gap-2">
+            <h2 className="text-lg font-bold">Minha lista de presentes</h2>
+            <div className="bg-muted flex min-h-40 w-full items-center justify-center rounded-md">
+              <p>Lista de presentes vazia</p>
+            </div>
+          </article>
+        </section>
+        <section className="w-full md:w-1/3">
+          <article className="grid w-full gap-2">
+            <h2 className="text-lg font-bold">Participantes</h2>
+            <div className="bg-muted flex min-h-40 w-full items-center justify-center rounded-md">
+              <p>N/A</p>
+            </div>
+          </article>
+        </section>
+      </main>
+    </>
+  )
 }
