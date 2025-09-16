1a18ee7144efb335c21e2e3ef6cc7cf6fdcdfade
Etore Grande <etoregrande@gmail.com>
2025-09-16 13:50:35 -0300

    style(shared): style shared wishlist

--- src/app/(auth)/wishlist/shared/[userId]/page.tsx
+++ src/app/(auth)/wishlist/shared/[userId]/page.tsx
@@ -2,11 +2,12 @@ import { listWishlistItems } from '@/server/wishlistItem'
 import { WishlistShared } from './ui/Wishlist-shared'
 import { getUserById } from '@/lib/repositories/UserRepository'
 import { WishlistItem } from '@/generated/prisma'
-import { getDisplayName } from '@/lib/utils'
+import { getDisplayName, setAvatarFallbackString } from '@/lib/utils'
 import { Session } from '@/lib/auth'
 import { getSessionOnServer } from '@/server/session'
 import { WishlistSharedOwnerWarning } from './ui/Wishlist-shared-owner-warning'
 import { Suspense } from 'react'
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 
 interface SharedWishlistParams {
   params: Promise<{ userId: string }>
@@ -21,20 +22,35 @@ export default async function SharedWishlistPage({
   const session: Session | null = await getSessionOnServer()
   const isOwner = session?.user.id === userId
 
+  const wishlistOwnerImage = wishlistOwner?.image ?? ''
   if (!wishlistOwner) throw new Error('Error fetching wishlist owner')
 
   return (
-    <Suspense fallback="loading">
-      <WishlistSharedOwnerWarning isOwner={isOwner}>
-        <div className="pb-16 lg:pb-0">
-          <h1 className="block px-4 pt-10 pb-16 text-4xl leading-8 font-black tracking-tight break-words md:px-8 lg:px-0">
-            {getDisplayName(wishlistOwner.name)}
-          </h1>
-          <div className="px-4 md:px-8 lg:px-0">
+    <main className="layout-container pb-[60dvh] md:pb-[10dvh]">
+      <Suspense fallback="loading">
+        <WishlistSharedOwnerWarning isOwner={isOwner}>
+          <div className="pb-16 lg:pb-0">
+            <div className="ml-2 flex items-center gap-6 pt-10 pb-16">
+              <Avatar className="outline-foreground size-16 outline-4 outline-offset-4 md:size-24">
+                <AvatarImage src={wishlistOwnerImage} />
+                <AvatarFallback className="bg-white font-bold">
+                  <p className="min-w-0 truncate text-2xl">
+                    {setAvatarFallbackString(wishlistOwner.name)}
+                  </p>
+                </AvatarFallback>
+              </Avatar>
+              <h1 className="block text-4xl leading-8 font-black tracking-tight break-words">
+                <span className="font-lg text-muted-foreground block text-lg font-normal">
+                  Lista de presentes de
+                </span>
+                {getDisplayName(wishlistOwner.name)}
+              </h1>
+            </div>
+
             <WishlistShared initialWishlist={wishlistItems} />
           </div>
-        </div>
-      </WishlistSharedOwnerWarning>
-    </Suspense>
+        </WishlistSharedOwnerWarning>
+      </Suspense>
+    </main>
   )
 }
