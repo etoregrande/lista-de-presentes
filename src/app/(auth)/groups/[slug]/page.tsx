14089d4f0f3bdadf3cc6bc1b42f114ed3ef26e9a
Etore Grande <etoregrande@gmail.com>
2025-09-17 14:41:17 -0300

    feat(secret-santa-group): add priceLimit and eventDate on secret santa group page

--- src/app/(auth)/groups/[slug]/page.tsx
+++ src/app/(auth)/groups/[slug]/page.tsx
@@ -11,7 +11,12 @@ import {
 import { LeaveSecretSantaGroupButton } from './ui/leave-secret-santa-group-button'
 import { getSecretSantaReceiver } from '@/server/secretSantaDraw'
 import { Button } from '@/components/ui/button'
-import { Settings, Trash } from 'lucide-react'
+import {
+  Calendar as CalendarIcon,
+  Settings as SettingsIcon,
+  Trash as TrashIcon,
+  Gift as GiftIcon,
+} from 'lucide-react'
 import { EditSecretSantaGroupSheet } from './ui/edit-secret-santa-group-sheet'
 import { SecretSantaGroupProvider } from './context/provider'
 import { ParticipantList } from './ui/participant/participant-list'
@@ -19,6 +24,9 @@ import { SecretSantaGroupBackground } from './ui/secret-santa-group-background'
 import { ShareSecretSantaGroupButton } from './ui/share-secret-santa-group-button'
 import { SecretSantaDrawResult } from './ui/secret-santa-draw-result'
 import { SecretSantaGroupName } from './ui/secret-santa-group-name'
+import { format } from 'date-fns'
+import { ptBR } from 'date-fns/locale'
+import { formatCurrencyFromCents } from '@/lib/utils'
 
 interface PageProps {
   params: Promise<{ slug: string }>
@@ -40,13 +48,13 @@ export default async function Page({ params }: PageProps) {
 
   const { id: userId } = session.user
   const group = await getSecretSantaGroup(slug)
-  const { id: groupId, ownerId } = group
+  const { id: groupId, ownerId, eventDate, priceLimit } = group
   const isOwner = userId === ownerId
   const SecretSantaDrawReceiver = await getSecretSantaReceiver(userId, groupId)
   const participants = await listSecretSantaGroupParticipants(groupId)
-  // const wishlist = await listWishlistItems(userId, {
-  //   ...(group.priceLimit && { maxPrice: group.priceLimit }),
-  // })
+  const formatedEventDate = eventDate
+    ? format(eventDate, "dd 'de' MMMM 'de' yyyy", { locale: ptBR })
+    : null
 
   return (
     <SecretSantaGroupProvider
@@ -55,31 +63,54 @@ export default async function Page({ params }: PageProps) {
       isOwner={isOwner}
     >
       <SecretSantaGroupBackground />
-      <header className="layout-container flex w-full items-center gap-8 pt-0 pb-10 md:pt-10">
-        <div className="min-w-0 flex-1">
-          <SecretSantaGroupName truncate={true} />
-        </div>
-        {isOwner && (
-          <div className="flex flex-shrink-0 gap-2">
-            <EditSecretSantaGroupSheet>
-              <Button variant={'secondary'} size={'icontext'}>
-                <Settings />
-                <span className="hidden md:inline">Editar grupo</span>
-              </Button>
-            </EditSecretSantaGroupSheet>
-            <ShareSecretSantaGroupButton />
+      <header className="layout-container flex w-full flex-col gap-4 pt-0 pb-10 md:pt-10">
+        <section className="flex items-center">
+          <div className="min-w-0 flex-1">
+            <SecretSantaGroupName truncate={true} />
           </div>
-        )}
-        {!isOwner && (
-          <div className="flex-shrink-0">
-            <LeaveSecretSantaGroupButton>
-              <Button variant={'destructive'} size={'icontext'}>
-                <Trash />
-                <span className="hidden md:inline">Abandonar grupo</span>
-              </Button>
-            </LeaveSecretSantaGroupButton>
+          {isOwner && (
+            <div className="flex flex-shrink-0 gap-2">
+              <EditSecretSantaGroupSheet>
+                <Button variant={'secondary'} size={'icontext'}>
+                  <SettingsIcon />
+                  <span className="hidden md:inline">Editar grupo</span>
+                </Button>
+              </EditSecretSantaGroupSheet>
+              <ShareSecretSantaGroupButton />
+            </div>
+          )}
+          {!isOwner && (
+            <div className="flex-shrink-0">
+              <LeaveSecretSantaGroupButton>
+                <Button variant={'destructive'} size={'icontext'}>
+                  <TrashIcon />
+                  <span className="hidden md:inline">Abandonar grupo</span>
+                </Button>
+              </LeaveSecretSantaGroupButton>
+            </div>
+          )}
+        </section>
+        <section>
+          <div className="flex gap-10">
+            {eventDate && (
+              <div className="flex gap-2">
+                <CalendarIcon className="text-primary" />
+                <p className="text-muted-foreground translate-y-[1px]">
+                  {formatedEventDate}
+                </p>
+              </div>
+            )}
+
+            {priceLimit && (
+              <div className="flex gap-2">
+                <GiftIcon className="text-primary" />
+                <p className="text-muted-foreground translate-y-[1px]">
+                  Presentes até {formatCurrencyFromCents(priceLimit)}
+                </p>
+              </div>
+            )}
           </div>
-        )}
+        </section>
       </header>
 
       <main className="layout-container flex flex-col gap-10 pb-[60dvh] md:pb-0">
@@ -96,13 +127,6 @@ export default async function Page({ params }: PageProps) {
               <ParticipantList />
             </div>
           </article>
-          {/* <article className="flex min-h-10 w-full flex-col gap-2">
-            <h2 className="text-lg font-bold">Minha lista de presentes</h2>
-           <EmptyWishlist isEmpty={true} /> 
-            <div className="grid gap-2 md:grid-cols-3 lg:grid-cols-4">
-              <SecretSantaWishlist items={wishlist} maxItems={8} />
-            </div>
-          </article> */}
         </div>
       </main>
     </SecretSantaGroupProvider>
